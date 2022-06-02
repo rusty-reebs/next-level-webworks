7e02dadc5971e5f3ca1b035b0cb9de803d75bb28
rusty-reebs <rusty_reebs@tutanota.com>
2022-06-02 16:25:32 -0600

    Add project description

--- src/components/utils/projects.js
+++ src/components/utils/projects.js
@@ -10,16 +10,15 @@ const projects = [
       "An Airbnb-style listing of worldschooling events. Users can browse events, click for details, and see locations on an interactive map.",
     link: "worldschoolingpopups.com",
     url: "https://worldschoolingpopups.com",
-    // image: "../../img/ws-screenshot.png",
     image: wsScreenshot,
     landscape: false,
   },
   {
     title: "Store Inventory Web App",
-    description: "Yada Yada",
+    description:
+      "An inventory management app with tag navigation and a cloud database. Users can browse, add, and delete items, categories, manufacturers, and countries of origin.",
     link: "https://crud-app-rusty-reebs.herokuapp.com/",
     url: "https://crud-app-rusty-reebs.herokuapp.com/",
-    // image: "../../img/cyclelogical.png",
     image: cycleLogical,
     landscape: true,
   },
